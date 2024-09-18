a5dba699e675b65ad1fdca4e41e2090261b5c1c3
Reza Opdebeeck <reza@aupluriel.be>
2024-09-18 12:39:02 +0200

    adding header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,18 @@
 
 @section('content')
 
+<section class="max-w-screen-xl mx-auto px-2 border-b-2 border-black py-24 flex justify-end">
+    <div class="uppercase font-medium border-2 border-black">
+        <h1 class="text-3xl p-2 text-center">Les privilèges invisibles</h1>
+        <h2 class="text-6xl text-center p-4 border-y-2 border-black"><span class="text-3xl">N°</span> x /250</h2>
+        <div class="flex">
+            <div class="border-r-2 border-black p-4 text-right">
+                <h3 class="text-xl">Étude socio-artistique</h3>
+                <h3 class="text-xl mt-2">Barbara Iweins</h3>
+            </div>
+            <h3 class="p-4 text-xl mt-2">2024/205</h3>
+        </div>
+    </div>
+</section>
+
 @endsection

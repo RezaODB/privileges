08581b53caa6df1ae8c5d572c6f1d8dae8535935
Reza Opdebeeck <reza@aupluriel.be>
2024-09-19 14:09:31 +0200

    adding logic to register

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,26 @@
 
 <section class="max-w-screen-xl mx-auto px-2 border-b-2 border-black py-12 sm:py-24 grid sm:grid-cols-2 gap-8 items-start">
     @guest
+    @if (request()->query('login') === 'yes')
+    <form action="{{ route('login') }}" method="post" class="grid grid-cols-1 gap-4 font-mono max-w-md">
+        @csrf
+        <input type="email" name="email" id="email" placeholder="Email" value="{{ old('email') }}" class="border-none w-full focus:ring-0" required>
+        @error('email')<div class="text-red-500">{{ $message }}</div>@enderror
+        <input type="password" name="password" id="password" placeholder="Mot de passe" class="border-none w-full focus:ring-0" required>
+        @error('password')<div class="text-red-500">{{ $message }}</div>@enderror
+        <input type="hidden" name="remember" value="yes">
+        <div class="flex gap-4 items-center">
+            <button type="submit" class="bg-black text-white py-2 px-4 uppercase">Se connecter</button>
+            <a href="{{ route('index', ['login' => 'no']) }}" class="underline">S'inscrire?</a>
+        </div>
+    </form>    
+    @else
     <form action="{{ route('register') }}" method="post" class="grid grid-cols-1 gap-4 font-mono max-w-md">
         @csrf
-        <input type="text" name="lastname" id="lastname" placeholder="Nom" value="{{ old('lastname') }}" class="border-none w-full focus:ring-0" required autofocus>
-        @error('lastname')<div class="text-red-500">{{ $message }}</div>@enderror
         <input type="text" name="name" id="name" placeholder="Prénom" value="{{ old('name') }}" class="border-none w-full focus:ring-0" required>
         @error('name')<div class="text-red-500">{{ $message }}</div>@enderror
+        <input type="text" name="lastname" id="lastname" placeholder="Nom" value="{{ old('lastname') }}" class="border-none w-full focus:ring-0" required autofocus>
+        @error('lastname')<div class="text-red-500">{{ $message }}</div>@enderror
         <input type="email" name="email" id="email" placeholder="Email" value="{{ old('email') }}" class="border-none w-full focus:ring-0" required>
         @error('email')<div class="text-red-500">{{ $message }}</div>@enderror
         <input type="password" name="password" id="password" placeholder="Mot de passe" class="border-none w-full focus:ring-0" required>
@@ -18,17 +32,23 @@
         @error('password_confirmation')<div class="text-red-500">{{ $message }}</div>@enderror
         <div class="flex gap-4 items-center">
             <button type="submit" class="bg-black text-white py-2 px-4 uppercase">Soumettre</button>
-            <a href="" class="underline">Déjà inscrit?</a>
+            <a href="{{ route('index', ['login' => 'yes']) }}" class="underline">Déjà inscrit?</a>
         </div>
-    </form>
+    </form>   
+    @endif
     @endguest
-
     @auth
     <ul class="font-mono max-w-md p-4 bg-white">
         <li>Nom: {{ Auth::user()->lastname }}</li>
         <li>Prénom: {{ Auth::user()->name }}</li>
         <li>Email: {{ Auth::user()->email }}</li>
-    </ul>    
+        <li>
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button class="underline mt-4">Me déconnecter</button>    
+            </form> 
+        </li>
+    </ul>   
     @endauth
 
     <div class="uppercase font-medium border-2 border-black max-w-md ml-auto">

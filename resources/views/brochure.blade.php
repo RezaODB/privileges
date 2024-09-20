dac1c18f26f69a90b12b075fdac7afec224e64aa
Reza Opdebeeck <reza@aupluriel.be>
2024-09-20 15:55:24 +0200

    adding front to brochure

--- resources/views/brochure.blade.php
+++ resources/views/brochure.blade.php
@@ -12,7 +12,7 @@
             <div class="w-4 h-4 rounded-full bg-black shrink-0 ml-auto"></div>
         </button>
         <div class="py-8" x-show="open === {{ $loop->index }}" x-transition.duration.500ms>
-            <h1 class="uppercase text-3xl sm:text-5xl lg:text-7xl font-medium mb-8">{{ $item->title }}</h1>
+            <h1 class="uppercase text-4xl sm:text-5xl lg:text-7xl font-medium mb-8">{{ $item->title }}</h1>
             <div class="prose font-serif marker:text-black columns-md max-w-none [column-rule:solid] gap-16 prose-h2:border-b-4 prose-h2:border-black prose-h2:font-sans prose-h3:font-sans prose-h3:font-normal">{!! $item->body !!}</div>
         </div>
     </div>

b2fbb8fc5bbf880ff15ca5754f3eb3e2d1257dce
Reza Opdebeeck <reza@aupluriel.be>
2024-10-04 11:39:16 +0200

    export questions to csv

--- resources/views/livewire/quotas.blade.php
+++ resources/views/livewire/quotas.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <section class="py-12 sm:px-4 flex flex-wrap-reverse justify-between items-end gap-8">
-        <h1 class="text-5xl font-bold uppercase border-t-8 border-black pt-2">Privileges<br>invisibles</h1>
+    <section class="flex flex-wrap justify-between items-start gap-8">
+        <h1 class="text-6xl uppercase font-title border-t-8 border-black pt-2">Privileges<br>invisibles</h1>
         <h2 class="text-4xl relative px-4 py-2">
             <span class="rounded-full w-2 h-2 bg-black absolute top-0 left-0"></span>
             <span class="rounded-full w-2 h-2 bg-black absolute top-0 right-0"></span>
@@ -10,10 +10,10 @@
         </h2>
     </section>
 
-    <form wire:submit class="py-12 sm:px-4 grid grid-cols-[auto_1fr_auto]">
+    <form wire:submit class="py-12 grid grid-cols-[auto_1fr_auto]">
         <div></div>
         <div class="border-b-4 border-black ml-4"></div>
-        <div class="border-b-4 border-black text-3xl uppercase font-medium pb-8">
+        <div class="border-b-4 border-black text-3xl uppercase font-bold pb-8">
             <div class="-rotate-90">
                 <h3>{{ __('content.true') }}</h3>
                 <h3 class="mt-4">{{ __('content.false') }}</h3>
@@ -21,10 +21,10 @@
         </div>
         @foreach ($quotas as $item)
         <div class="font-bold pt-4 px-4">{{ sprintf('%03d', $loop->iteration) }}</div>
-        <div class="font-print text-lg pt-4 px-4">{{ $item->{'question_' . app()->getLocale()} }}</div>
+        <div class="font-mono pt-4 px-4">{{ $item->{'question_' . app()->getLocale()} }}</div>
         <div class="border-l-4 border-black pt-6 px-4 flex gap-4">
-            <input type="radio" wire:model.blur="answers.{{ $loop->iteration }}" value="yes" class="rounded-none border-none w-8 h-3 text-black checked:bg-none focus:ring-0"> 
-            <input type="radio" wire:model.blur="answers.{{ $loop->iteration }}" value="no" class="rounded-none border-none w-8 h-3 text-black checked:bg-none focus:ring-0"> 
+            <input type="radio" wire:model.blur="answers.{{ $loop->iteration }}" value="yes" class="rounded-none w-8 h-3 text-black checked:bg-none focus:ring-0"> 
+            <input type="radio" wire:model.blur="answers.{{ $loop->iteration }}" value="no" class="rounded-none w-8 h-3 text-black checked:bg-none focus:ring-0"> 
         </div>
         @endforeach
     </form>
--- resources/views/step2.blade.php
+++ resources/views/step2.blade.php
@@ -2,4 +2,23 @@
 
 @section('content')
 
+<section class="bg-white px-4 md:px-8 py-16 shadow-xl">
+
+    @guest
+    <div class="max-w-md font-mono">
+        @if (App::isLocale('en'))
+        Please <a href="{{ route('index', ['login' => 'yes']) }}" class="underline">sign in</a> before proceeding to the questionnaire.
+        @endif
+        @if (App::isLocale('fr'))
+        Merci de vous <a href="{{ route('index', ['login' => 'yes']) }}" class="underline">connecter à votre compte</a> afin de répondre au questionnaire.
+        @endif
+    </div>
+    @endguest
+    
+    @auth
+    @livewire('quotas')
+    @endauth
+    
+</section>
+
 @endsection

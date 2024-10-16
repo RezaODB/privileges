a42b883ed0f4bcb730f36dcc589a9b1ea53a3571
Reza Opdebeeck <reza@aupluriel.be>
2024-10-16 11:15:47 +0200

    adding cross

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,7 +7,7 @@
                         @foreach ($users as $item)
                         <div class="flex gap-4 items-center p-1 hover:bg-gray-100">
                             <span class="font-bold text-gray-600">{{ $loop->iteration }}</span>
-                            <h1>{{ $item->name . ' ' . $item->lastname }}</h1>
+                            <h1>{{ $item->lastname . ' ' . $item->name }}</h1>
                             <span class="bg-green-100 text-green-600 uppercase px-2 whitespace-nowrap rounded text-sm">Réponses: {{ count($item->answers->answers) }}/{{ $total }}</span>
                             <span class="bg-blue-100 text-blue-600 uppercase px-2 whitespace-nowrap rounded text-sm">Quota: {{ round(data_get(array_count_values($item->answers->answers), 'yes') / $total * 100) }}%</span>
                             <form action="{{ route('users.destroy', $item) }}" method="post" class="ml-auto">

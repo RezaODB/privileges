c2e39c5b1d86bc308c01d7dada4983f961c71d1d
Reza Opdebeeck <reza@aupluriel.be>
2024-10-17 12:42:35 +0200

    minor tweaks

--- resources/views/step1.blade.php
+++ resources/views/step1.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<section class="bg-stone-100 pl-4 md:pl-8 py-16 shadow-xl">
+<section class="bg-stone-100 pl-4 md:pl-8 py-16 shadow-3xl">
     @foreach ($theories as $item)
     <div class="cursor-pointer border-b border-black mb-8" x-data="{ open: false }" x-on:click="open = !open">
         <div class="flex items-center justify-between gap-4 pr-4 md:pr-8">
@@ -14,7 +14,7 @@
         <div class="mt-8 overflow-hidden relative pb-32" x-bind:class="open ? 'h-full' : 'h-0'">
             <div class="grid grid-cols-1 md:grid-cols-3 items-end gap-16">
                 <div class="prose prose-p:font-bold max-w-none order-2 md:order-1">{!! $item->quotes !!}</div>
-                <div class="md:col-span-2 prose prose-img:ml-auto prose-img:-mr-4 sm:prose-img:-mr-8 prose-img:shadow-lg max-w-none text-justify font-serif order-1 md:order-2 mr-4 sm:mr-8">{!! $item->body !!}</div>
+                <div class="md:col-span-2 prose prose-img:ml-auto prose-img:-mr-4 sm:prose-img:-mr-8 prose-img:shadow-xl max-w-none text-justify font-serif order-1 md:order-2 mr-4 sm:mr-8">{!! $item->body !!}</div>
             </div>
             <div class="bg-gradient-to-t from-stone-100 h-32 absolute bottom-0 w-full"></div>
         </div>

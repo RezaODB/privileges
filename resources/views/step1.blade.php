9bab60740a5d2c297a31e4108d240d5bc2649d38
Reza Opdebeeck <reza@aupluriel.be>
2024-10-22 11:45:36 +0200

    tweaking

--- resources/views/step1.blade.php
+++ resources/views/step1.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <section class="bg-white py-16 px-4 sm:px-8 shadow-xl relative">
-    <div class="absolute inset-0 mix-blend-luminosity pointer-events-none brightness-110" style="background-image: url({{ asset('paper.png') }}); background-size: 100% auto"></div>
+    <div class="absolute inset-0 pointer-events-none" style="background-image: url({{ asset('textures/paperw.png') }}); background-size: 100% auto"></div>
     <h1 class="relative text-2xl sm:text-4xl font-medium uppercase mb-12">Étude théorique</h1>
     @foreach ($theories as $item)
     <div class="font-mono relative mb-4" x-data="{ open: false }">

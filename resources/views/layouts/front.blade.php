8349530b2efd717399c497e5937a640cb1f23eeb
Reza Opdebeeck <reza@aupluriel.be>
2024-10-24 23:22:39 +0200

    deleting myself

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -26,8 +26,8 @@
     </style>
     <body class="font-sans antialiased bg-gray-100">
         <div class="max-w-screen-2xl mx-auto sm:px-4">
-            <div class="min-h-screen pb-16 sm:my-4 bg-yellow-100 shadow-2xl relative rounded-t-xl overflow-hidden">
-                <div class="absolute inset-0 pointer-events-none" style="background-image: url({{ asset('paper.png') }}); background-size: 100% auto;"></div>
+            <div class="min-h-screen pb-16 sm:my-4 bg-[#e5d0b5] shadow-2xl relative rounded-t-xl rounded-b-md overflow-hidden">
+                <div class="absolute inset-0 pointer-events-none" style="background-image: url({{ asset('folder.png') }}); background-size: 100% auto;"></div>
                 <header class="relative">
                     @include('includes.header')
                 </header>

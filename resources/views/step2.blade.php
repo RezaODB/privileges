cd3623c211c0547633f17689f66ce94fa30e76d9
Reza Opdebeeck <reza@aupluriel.be>
2024-10-25 13:20:43 +0200

    deleting myself

--- resources/views/step2.blade.php
+++ resources/views/step2.blade.php
@@ -2,10 +2,8 @@
 
 @section('content')
 
-<section class="bg-white py-16 px-4 sm:px-8 shadow-xl relative">
-    <div class="absolute inset-0 mix-blend-luminosity pointer-events-none brightness-110" style="background-image: url({{ asset('paper.png') }}); background-size: 100% auto"></div>
-    <h1 class="relative text-2xl sm:text-4xl font-medium uppercase mb-12">{{ __('content.step2') }}</h1>
-    
+<section class="bg-[#fdf2e3] p-2 pb-16 shadow-xl">
+    <h2 class="text-right text-2xl sm:text-4xl font-light mb-8">{{ Auth::id() ?? 'X' }}/250</h2>
     @guest
     <div class="max-w-md font-mono relative">
         @if (App::isLocale('en'))

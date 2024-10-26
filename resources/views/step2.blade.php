703eb5a58c10a9c7169481afb19d118aa19a81cf
Reza Opdebeeck <reza@aupluriel.be>
2024-10-26 11:50:12 +0200

    tweak

--- resources/views/step2.blade.php
+++ resources/views/step2.blade.php
@@ -5,7 +5,7 @@
 <section class="bg-[#fdf2e3] p-2 pb-16 shadow-xl">
     <h2 class="text-right text-2xl sm:text-4xl font-light mb-8">{{ Auth::id() ?? 'X' }}/250</h2>
     @guest
-    <div class="max-w-md font-mono relative">
+    <div class="max-w-md font-mono px-4">
         @if (App::isLocale('en'))
         Please <a href="{{ route('index', ['login' => 'yes']) }}" class="underline">sign in</a> before proceeding to the questionnaire.
         @endif

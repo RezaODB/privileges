91d702ea849da5fa5345af0273de19580cf0327b
Reza Opdebeeck <reza@aupluriel.be>
2024-11-03 10:46:07 +0100

    Retours barbara index

--- resources/views/step2.blade.php
+++ resources/views/step2.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<section class="bg-[#fdf2e3] p-2 pb-16 shadow-xl">
+<section class="bg-[#fdf2e3] p-2 pb-16 shadow-xl rounded-3xl">
     <h2 class="text-right text-2xl sm:text-4xl font-light mb-8">{{ Auth::id() ?? 'X' }}/250</h2>
     @guest
     <div class="max-w-md font-mono px-4">

69a5aa4243405f6d943ccd2f756315898d11dd9c
Reza Opdebeeck <reza@aupluriel.be>
2024-11-28 16:23:06 +0100

    debug pourcentage

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,7 +11,7 @@
                             <a href="mailto:{{ $item->email }}" class="underline">{{ $item->email }}</a>
                             <a href="tel:{{ $item->phone }}" class="underline">{{ $item->phone }}</a>
                             <span class="bg-slate-100 text-slate-600 uppercase px-2 whitespace-nowrap rounded text-sm">Réponses: {{ count($item->answers->answers) - array_key_exists('comment', $item->answers->answers) }}/{{ $quotas }}</span>
-                            <span class="bg-slate-300 text-slate-800 uppercase px-2 whitespace-nowrap rounded text-sm">Quota: {{ round(data_get(array_count_values($item->answers->answers), 'yes') / $quotas * 100) }}%</span>
+                            <span class="bg-slate-300 text-slate-800 uppercase px-2 whitespace-nowrap rounded text-sm">Quota: {{ round(data_get(array_count_values($item->answers->answers), 'no') / $quotas * 100) }}%</span>
                             <span class="bg-slate-100 text-slate-600 uppercase px-2 whitespace-nowrap rounded text-sm">Votes: {{ count($item->answers->votes) - array_key_exists('comment', $item->answers->votes) }}/{{ $votes }}</span>
                             <span class="bg-slate-300 text-slate-800 uppercase px-2 whitespace-nowrap rounded text-sm">Loterie: {{ data_get(array_count_values($item->answers->votes), 'yes') ?? 0 }} POUR</span>
                             <form action="{{ route('users.destroy', $item) }}" method="post" class="ml-auto">

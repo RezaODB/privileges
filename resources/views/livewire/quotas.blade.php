e53f3bb7eaf38d58fb37af53e6cf23c38fa6f9af
Reza Opdebeeck <reza@aupluriel.be>
2024-12-08 19:45:17 +0100

    headings firefox

--- resources/views/livewire/quotas.blade.php
+++ resources/views/livewire/quotas.blade.php
@@ -10,13 +10,13 @@
         {{ $item->{'question_' . app()->getLocale()} }}
     </div>
     <div class="py-3 border-b-2 border-zinc-800 border-dotted flex items-center justify-end gap-4 ml-px">
-        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="yes" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0"> 
-        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="no" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0"> 
+        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="yes" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled> 
+        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="no" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled> 
     </div>
     @endforeach
     <div class="flex flex-col lg:flex-row gap-x-6 gap-y-4 mt-12 col-span-2">
         <label for="comment" class="text-3xl uppercase">{{ __('content.comment') }}</label>
-        <textarea wire:model.live.debounce.1000ms="answers.comment" id="comment" rows="5" class="w-full h-full focus:ring-0"></textarea>
+        <textarea wire:model.live.debounce.1000ms="answers.comment" id="comment" rows="5" class="w-full h-full focus:ring-0 disabled:bg-[#fdf2e3]" disabled></textarea>
     </div>
 </form>
 
--- resources/views/livewire/votes.blade.php
+++ resources/views/livewire/votes.blade.php
@@ -8,15 +8,15 @@
     @foreach ($questions as $item)
     <div class="font-mono py-3 border-b-2 border-zinc-800 border-dotted pr-4">{{ $item->{'question_' . app()->getLocale()} }}</div>
     <div class="py-3 border-b-2 border-zinc-800 border-dotted flex items-center justify-end gap-4 ml-px">
-        <input type="radio" wire:model.live="votes.{{ $item->id }}" value="yes" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0"> 
-        <input type="radio" wire:model.live="votes.{{ $item->id }}" value="no" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0">
-        <div class="group hover:scale-150 duration-700" wire:click="$set('votes.{{ $item->id }}', '{{ rand(0, 1) === 1 ? 'yes' : 'no' }}')">
+        <input type="radio" wire:model.live="votes.{{ $item->id }}" value="yes" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled> 
+        <input type="radio" wire:model.live="votes.{{ $item->id }}" value="no" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled>
+        <div class="group hover:scale-150 duration-700 hidden" wire:click="$set('votes.{{ $item->id }}', '{{ rand(0, 1) === 1 ? 'yes' : 'no' }}')">
             <div class="w-6 h-6 border border-zinc-800 rounded-full bg-white hover:bg-zinc-800 duration-700 cursor-pointer group-hover:[transform:rotateX(540deg)]"></div>
         </div>
     </div>
     @endforeach
     <div class="flex flex-col lg:flex-row gap-x-6 gap-y-4 mt-12 col-span-2">
         <label for="comment" class="text-3xl uppercase">{{ __('content.comment') }}</label>
-        <textarea wire:model.live.debounce.1000ms="votes.comment" id="comment" rows="5" class="w-full h-full focus:ring-0"></textarea>
+        <textarea wire:model.live.debounce.1000ms="votes.comment" id="comment" rows="5" class="w-full h-full focus:ring-0 disabled:bg-[#fdf2e3]" disabled></textarea>
     </div>
 </form>

d117c42e2473d1047114a420d27f572dd4545c82
Reza Opdebeeck <reza@aupluriel.be>
2024-12-12 21:16:16 +0100

    tweaking users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,14 +5,14 @@
                 <div class="p-6 text-gray-900">
                     <div class="divide-y">
                         @foreach ($users as $item)
-                        <div class="flex gap-4 items-center p-1 hover:bg-gray-100 overflow-scroll">
+                        <div class="flex gap-4 items-center p-1 hover:bg-gray-100 overflow-scroll text-sm">
                             <span class="font-bold text-gray-600">{{ $item->order }}</span>
                             <a href="{{ route('users.show', $item) }}" class="text-blue-600 whitespace-nowrap hover:underline">{{ $item->lastname . ' ' . $item->name }}</a>
                             <a href="mailto:{{ $item->email }}" class="whitespace-nowrap underline">{{ $item->email }}</a>
                             <a href="tel:{{ $item->phone }}" class="whitespace-nowrap underline">{{ $item->phone }}</a>
-                            <span class="bg-slate-100 text-slate-600 uppercase px-2 whitespace-nowrap rounded text-sm">Réponses: {{ count($item->answers->answers) - array_key_exists('comment', $item->answers->answers) }}/{{ $quotas }}</span>
+                            <span class="bg-slate-100 text-slate-600 uppercase px-2 whitespace-nowrap rounded">Réponses: {{ count($item->answers->answers) - array_key_exists('comment', $item->answers->answers) }}/{{ $quotas }}</span>
                             {{-- <span class="bg-slate-300 text-slate-800 uppercase px-2 whitespace-nowrap rounded text-sm">Quota: {{ round(data_get(array_count_values($item->answers->answers), 'no') / $quotas * 100) }}%</span> --}}
-                            <span class="bg-slate-100 text-slate-600 uppercase px-2 whitespace-nowrap rounded text-sm">Votes: {{ count($item->answers->votes) - array_key_exists('comment', $item->answers->votes) }}/{{ $votes }}</span>
+                            <span class="bg-slate-100 text-slate-600 uppercase px-2 whitespace-nowrap rounded">Votes: {{ count($item->answers->votes) - array_key_exists('comment', $item->answers->votes) }}/{{ $votes }}</span>
                             {{-- <span class="bg-slate-300 text-slate-800 uppercase px-2 whitespace-nowrap rounded text-sm">Loterie: {{ data_get(array_count_values($item->answers->votes), 'yes') ?? 0 }} POUR</span> --}}
                             <form action="{{ route('users.destroy', $item) }}" method="post" class="ml-auto">
                                 @csrf

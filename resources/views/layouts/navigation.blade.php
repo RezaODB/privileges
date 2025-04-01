14b1f622d6586f040f92cff16a92d3df699189a7
Reza Opdebeeck <reza@aupluriel.be>
2025-04-01 13:29:42 +0200

    new labels for steps

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,14 +5,11 @@
             <div class="flex overflow-auto">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
+                    <a href="{{ route('index') }}">Site</a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">{{ __('Dashboard') }}</x-nav-link>
                     <x-nav-link :href="route('users.index')" class="whitespace-nowrap" :active="request()->routeIs('users.index')">Participants</x-nav-link>
                     <x-nav-link :href="route('theories.index')" class="whitespace-nowrap" :active="request()->routeIs('theories.index')">Théorie</x-nav-link>
                     <x-nav-link :href="route('intros.index')" class="whitespace-nowrap" :active="request()->routeIs('intros.index')">Quota</x-nav-link>
@@ -75,7 +72,6 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">{{ __('Dashboard') }}</x-responsive-nav-link>
             <x-responsive-nav-link :href="route('users.index')" class="whitespace-nowrap" :active="request()->routeIs('users.index')">Participants</x-responsive-nav-link>
             <x-responsive-nav-link :href="route('theories.index')" class="whitespace-nowrap" :active="request()->routeIs('theories.index')">Théorie</x-responsive-nav-link>
             <x-responsive-nav-link :href="route('intros.index')" class="whitespace-nowrap" :active="request()->routeIs('intros.index')">Quota</x-responsive-nav-link>

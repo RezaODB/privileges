00cf8154fcc29fdfc270d2d09f63cb471636d9b4
Reza Opdebeeck <reza@aupluriel.be>
2025-04-07 15:42:08 +0200

    adding booster

--- app/Livewire/Quotas.php
+++ app/Livewire/Quotas.php
@@ -10,14 +10,20 @@ class Quotas extends Component
 {
     public $answers = [];
 
+    public $boosters = []; 
+
     public function mount()
     {
         $this->answers = Auth::user()->answers->answers; 
+        $this->boosters = Auth::user()->answers->answers['boosters']; 
     }
 
     public function updated()
     {
-        Auth::user()->answers->update(['answers' => $this->answers]);
+        Auth::user()->answers->update([
+            'answers' => $this->answers,
+            'answers->boosters' => $this->boosters
+        ]);
     }
 
     public function render()
--- resources/views/livewire/quotas.blade.php
+++ resources/views/livewire/quotas.blade.php
@@ -6,6 +6,7 @@
         <h3 class="[writing-mode:vertical-lr]">{{ __('content.sometimes') }}</h3>
         <h3 class="[writing-mode:vertical-lr]">{{ __('content.rarely') }}</h3>
         <h3 class="[writing-mode:vertical-lr]">{{ __('content.never') }}</h3>
+        <h3 class="[writing-mode:vertical-lr]">BOOSTERS (3)</h3>
     </div>
     @foreach ($quotas as $item)
     <div class="font-mono py-3 border-b-2 border-zinc-800 border-dotted flex items-start gap-4 pr-4">
@@ -13,16 +14,16 @@
         {{ $item->{'question_' . app()->getLocale()} }}
     </div>
     <div class="py-3 border-b-2 border-zinc-800 border-dotted flex items-center justify-end gap-4 ml-px">
-        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="always" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled> 
-        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="often" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled> 
-        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="sometimes" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled> 
-        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="rarely" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled> 
-        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="never" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none" disabled> 
+        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="always" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none"> 
+        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="often" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none"> 
+        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="sometimes" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none"> 
+        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="rarely" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none"> 
+        <input type="radio" wire:model.live="answers.{{ $item->id }}" value="never" class="rounded-none w-8 h-3 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3] disabled:border-none"> 
+        <input type="checkbox" wire:model.live="boosters" value="{{ $item->id }}" class="rounded-full w-6 h-6 mx-2 text-zinc-800 checked:bg-none focus:ring-0 disabled:bg-[#fdf2e3]" {{ count($boosters) >= 3 && !in_array($item->id, $boosters) ? 'disabled' : '' }}> 
     </div>
     @endforeach
     <div class="flex flex-col lg:flex-row gap-x-6 gap-y-4 mt-12 col-span-2">
         <label for="comment" class="text-3xl uppercase">{{ __('content.comment') }}</label>
-        <textarea wire:model.live.debounce.1000ms="answers.comment" id="comment" rows="5" class="w-full h-full focus:ring-0 disabled:bg-[#fdf2e3]" disabled></textarea>
+        <textarea wire:model.live.debounce.1000ms="answers.comment" id="comment" rows="5" class="w-full h-full focus:ring-0 disabled:bg-[#fdf2e3]"></textarea>
     </div>
 </form>
-

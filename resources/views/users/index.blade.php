6bfa9f4e442bcda418c164845e2f906461a42c6f
Reza Opdebeeck <reza@aupluriel.be>
2026-02-13 15:43:54 +0100

    Constrain users table height for sticky header

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 space-y-4">
                     <a href="{{ route('users.export_users') }}" class="px-4 py-2 bg-gray-600 rounded-md text-white inline-block">Export to CSV</a>
-                    <div class="overflow-x-auto">
+                    <div class="overflow-x-auto overflow-y-auto max-h-[70vh]">
                         <table class="min-w-full divide-y divide-gray-200 text-sm">
                             <thead class="sticky top-0 z-20 bg-gray-50">
                                 <tr>

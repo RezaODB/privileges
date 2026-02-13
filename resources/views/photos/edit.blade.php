05a00625c2bc114facedca46b3be213c393dde37
Reza Opdebeeck <reza@aupluriel.be>
2026-02-13 16:53:42 +0100

    Apply pending Blade view updates

--- resources/views/photos/edit.blade.php
+++ resources/views/photos/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl 2xl:max-w-[1800px] mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 space-y-4">
                     <form action="{{ route('photos.update', $photo) }}" method="post">
